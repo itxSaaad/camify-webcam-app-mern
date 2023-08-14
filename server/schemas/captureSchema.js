ff31ce7d79a9994b1eb664f880de99cd889784cb
itxSaaad <saadstudent.cs@gmail.com>
2023-08-14 14:21:31 +0500

    Added: Mongo Schemas to server

---
+++ server/schemas/captureSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const captureSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Capture', captureSchema);
---
+++ server/schemas/userSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+    },
+    avatar: {
+      type: Image,
+      default: 'https://avatars.dicebear.com/api/avataaars/1.svg',
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
